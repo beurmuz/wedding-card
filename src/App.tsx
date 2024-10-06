65630e975b867fddf66ab332096c42b18d5aaec4
beurmuz <fallinta2@gmail.com>
2024-10-06 17:37:08 +0900

    데이터 가져오기

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,53 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
-
 import classNames from 'classnames/bind'
+import { useEffect, useState } from 'react'
 
 import styles from './App.module.scss'
+
+import FullScreenMessage from './componenets/shared/FullScreenMessage'
 
 const cx = classNames.bind(styles)
 
 function App() {
-  return (
-    <div className={cx('container')}>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>적용이 된건가용?</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+  const [wedding, setWedding] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+
+  // 1. wedding data 호출
+  //  - 2개의 서버를 먼저 켜 주어야 함
+  //    1) yarn dev:db -> JSON Server 켜기
+  //    2) yarn start -> 개발 환경 켜기
+  useEffect(() => {
+    setLoading(true)
+    // 데이터를 보장받는 방법 callback, promise, async/await
+    fetch('http://localhost:8888/wedding')
+      .then((response) => {
+        if (response.ok === false) {
+          throw new Error('정보를 불러오지 못했습니다.')
+        }
+
+        return response.json()
+      })
+      .then((data) => {
+        setWedding(data)
+      })
+      .catch((e) => {
+        console.log(e)
+        setError(true)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [])
+
+  if (loading) {
+    return <FullScreenMessage type="loading" />
+  }
+
+  if (error) {
+    return <FullScreenMessage type="error" />
+  }
+
+  return <div className={cx('container')}>{JSON.stringify(wedding)}</div>
 }
 
 export default App
