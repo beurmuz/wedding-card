aede82bf3b9df02a92ad9d36001cd64d4ca3e01a
beurmuz <fallinta2@gmail.com>
2024-10-12 19:28:20 +0900

    이미지 최적화

--- src/components/sections/Video.tsx
+++ src/components/sections/Video.tsx
@@ -8,14 +8,10 @@ const cx = classNames.bind(styles)
 function Video() {
   return (
     <Section className={cx('container')}>
-      <video
-        autoPlay={true}
-        muted={true}
-        loop={true}
-        poster="/assets/poster.jpg"
-      >
+      <video autoPlay muted loop poster="/assets/poster.jpg">
         {/* video tag에 controls={true}를 넣어주면 일시정지 및 이동이 가능해진다 */}
-        <source src="/assets/main.mp4"></source>
+        <source src="/assets/main.webm" type="video/webm" />
+        <source src="/assets/main.mp4" type="video/mp4" />
       </video>
     </Section>
   )
